582736be87d26ee15bb7a08e3ecfd55cc350efe4
it21032974 <>
2021-07-20 03:04:51 +0000

    completed

--- Tute02.c
+++ Tute02.c
@@ -15,10 +15,24 @@ Amount = 20 x 50 = 1000
 
 Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
-
 #include <stdio.h>
 
 int main() {
-  
+  int distance;
+  float amount;
+
+  printf("Enter Distance : ");
+  scanf("%d",&distance);
+
+  if(distance < 30)
+  {
+    amount = distance * 50;
+  }
+  else 
+  {
+    amount = 30 * 50+(distance - 30)*40;
+  }
+
+  printf("Amount is : %.2f",amount);
   return 0;
 }
--- Tute03.c
+++ Tute03.c
@@ -10,10 +10,20 @@ sum = 1+2+3+....+ 99+100 = 5050
 
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
-
 #include <stdio.h>
 int main() {
+  int num , count=1;
+  int sum = 0;
+
+  printf("Enter Number : ");
+  scanf("%d",&num);
+
+  while(count<= num)
+  {
+    sum = sum + count;
+    count++;
+  }
+  printf("Sum is : %d",sum);
   
   return 0;
 }
-
